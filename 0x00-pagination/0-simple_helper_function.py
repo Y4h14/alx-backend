d0a42b1f7b03f4b8119e84f9bfb31eff5db837ac
Y4h14 <yahyasadig2013@gmail.com>
2024-06-22 12:41:58 +0300

    initial commit

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" defines a fucntion that returns the range of response"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int)-> Tuple[int, int]:
+    """teh function that calculate the range of indexes
+
+    Args:
+        page (int): the number of the page
+        page_size (int): the size of the page
+
+    Returns:
+        Tuple[int, int]: a tuple of size two containing a start index and an end index
+          corresponding to the range of indexes 
+    """
+    return ((page - 1) * page_size, page_size * page)
