8d699a6fbabd20f3493990d9aa33b8f6cb982292
Y4h14 <yahyasadig2013@gmail.com>
2024-06-26 16:41:24 +0300

    tasks 0 and 1 initial commit

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""define a class for cache"""
+BaseCaching = __import__('BaseCaching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    """
+    def put(self, key, item):
+        """_summary_
+
+        Args:
+            key (str): cache item key
+            item (Any): item stored in cache
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+
+        Args:
+            key (str): the cache item key
+        """
+        return self.cache_data.get(key, None)
