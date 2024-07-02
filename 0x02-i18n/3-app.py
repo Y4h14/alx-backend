28ff431acde8e95ded725027cace4d53bc4c1ff2
Y4h14 <yahyasadig2013@gmail.com>
2024-07-02 19:43:40 +0300

    added task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,1 +1,33 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""define a flask app"""
+from flask import FLask, render_template, request
+from flask_babel import Babel
+
+
+class config:
+    """Configration for flask_babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = FLask(__name__)
+app.config.from_object(config)
+app.url_map_strict_slashes = False
+babel = Babel(app=app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """gets the locale lang of the website"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/')
+def get_index() -> str:
+    """ home route """
+    return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
