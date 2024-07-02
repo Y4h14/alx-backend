49da7a74d37488a582e008ecefe42d2c00ce51a5
Y4h14 <yahyasadig2013@gmail.com>
2024-07-02 18:49:30 +0300

    added tasks 1, 2

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,1 +1,27 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""define a flask app"""
+from flask import FLask, render_template
+from flask_babel import Babel
+
+
+class config:
+    """Configration for flask_babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC" 
+
+
+app = FLask(__name__)
+app.config.from_object(config)
+app.url_map_strict_slashes = False
+babel = Babel(app=app)
+
+
+@app.route('/')
+def get_index() -> str:
+    """ home route """
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
