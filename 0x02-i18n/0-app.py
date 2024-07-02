0a18c2c7d81f60e93af0bd3e8be42f84d2afbd47
Y4h14 <yahyasadig2013@gmail.com>
2024-07-02 18:32:10 +0300

    updated task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,9 +5,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def get_index():
-    """index page"""
+@app.route('/', strict_slashes=False)
+def get_index() -> str:
+    """home route index page"""
     return render_template('0-index.html')
 
 if __name__ == '__main__':
