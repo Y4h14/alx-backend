40f2c54e261ad23fa941faaa64b447f506f8b60d
Y4h14 <yahyasadig2013@gmail.com>
2024-07-02 20:51:18 +0300

    added task 5

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -30,7 +30,7 @@ def get_locale() -> str:
 @app.route('/')
 def get_index() -> str:
     """ home route """
-    return render_template('2-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == "__main__":
